401c7339953cfc9d5c0f79784fe1a1bf90049e9d
qwasa@qwasa.net <qwasa@qwasa.net>
2021-07-25 20:56:15 +0300

    python async client -- add pool size semaphore

--- asynchronous/client.py
+++ asynchronous/client.py
@@ -1,34 +1,48 @@
 """
-Async client impementation.
+Async client implementation.
 
-Unlimited concurrent connections.
+Semaphore-limited concurrent connections.
 """
 
 import asyncio
 import logging
 import socket
 
+from config import POOL_MAX_SIZE
+
+# counters for simultaneously running workers
 workers_running = 0
 workers_running_max = 0
 
+# semaphore to limit number of workers
+pool_tickets = None
+
 
 def run(connect: tuple, readers, limit) -> None:
-    """Run asyncio."""
+    """Run client, run."""
     asyncio.run(run_async(connect, readers, limit))
 
 
 async def run_async(connect, readers, limit):
     """Run asynchronous client. Unlimited concurrent connections."""
+    global pool_tickets
+
+    # init semaphore
+    pool_tickets = asyncio.Semaphore(POOL_MAX_SIZE)
+    loop = asyncio.get_event_loop()
 
     counter = 0
-
-    loop = asyncio.get_event_loop()
     tasks = []
 
     # create tasks
     while counter < limit:
+
+        # block here and wait for the slot in the pool
+        await pool_tickets.acquire()
+
         counter += 1
         task = loop.create_task(reader_wrapper(connect, readers, counter), name=f"task#{counter}")
+
         tasks.append(task)
 
     # wait tasks
@@ -37,8 +51,8 @@ async def run_async(connect, readers, limit):
 
 
 async def reader_wrapper(connect, readers, reader_id):
-    """Add some logging for worker."""
-    global workers_running, workers_running_max
+    """Add some stats and logging for the worker."""
+    global workers_running, workers_running_max, pool_tickets
 
     workers_running += 1
     workers_running_max = max(workers_running_max, workers_running)
@@ -49,14 +63,19 @@ async def reader_wrapper(connect, readers, reader_id):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.setblocking(False)
 
+    # connect to the server
     loop = asyncio.get_event_loop()
-
     await loop.sock_connect(client, connect)
+
+    # call the reader
     await readers.reader_async(client, reader_id)
 
     workers_running -= 1
     logging.info("worker %s / %s is done (%s running/%s max)", reader_id,
                  asyncio.current_task().get_name(), workers_running, workers_running_max)
+
+    # leave the pool here
+    pool_tickets.release()
 
 
 def stop():
